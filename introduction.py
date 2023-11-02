d96c5bb9f3eb4c862efd1e47bb80d6fa233612a1
adham khalil <adhamkhalil@Adhams-MBP.lan>
2023-11-01 23:39:47 -0400

    week 10 review

---
+++ introduction.py
@@ -0,0 +1,7 @@
+def introduction(name1, name2):
+    print(name1, " Meet ", name2)
+
+name1 = input("person 1, what is your name?")
+name2 = input("person 2, what is your name?")
+
+introduction(name1,name2)
